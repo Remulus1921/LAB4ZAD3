7c51a9a914eb9fd8e2a7f04216167d659955d6dd
Remigiusz Bekierz <remobekierz@onet.pl>
2022-04-19 21:44:08 +0200

    Nowa klasa Deskorolka

---
+++ Deskorolka.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4_zad3
+{
+    internal class Deskorolka : Pojazd
+    {
+        public int Rozmiar { get; set; }
+        public string Marka { get; set; }
+        public string DataPrzegladu { get; set; }
+        public override void KiedyPrzeglad()
+        {
+            Console.WriteLine("Przeglad dnia: " + DataPrzegladu);
+        }
+
+        public override void WyswietlObiekt()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
